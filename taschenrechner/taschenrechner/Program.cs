060af55a81c54e1bfc963b158f13a85fd49a737b
Michael Schulte-Cörne <michaelmbuso@schule.koeln>
2019-09-13 13:52:59 +0200

    Subtration hinzugefühgt

---
+++ taschenrechner/taschenrechner/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1;
+            int zahl2;
+            int auswahl;
+            int ergebnis = 0;
+            string zeichen="";
+
+            Console.WriteLine("Bitte wählen sie ein rechen zeichen aus.");
+            Console.WriteLine("1. +");
+            Console.WriteLine("2. -\n");
+            auswahl = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.Write("\nZahl 1: ");
+            zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Zahl 2: ");
+            zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            
+
+            switch (auswahl)
+            {
+                case 1:
+                    ergebnis = zahl1 + zahl2;
+                    zeichen = "+";
+                    break;
+                case 2:
+                    ergebnis = zahl1 - zahl2;
+                    zeichen = "-";
+                    break;
+                default:
+                    break;
+            }
+
+
+            Console.WriteLine(zahl1 + zeichen + zahl2 + " = " + ergebnis);
+
+            Console.ReadLine();
+
+         
+        }
+    }
+}
