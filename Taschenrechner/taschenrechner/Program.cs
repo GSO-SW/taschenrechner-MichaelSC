f19e51c8f86ef08e51e3cc7eeeee5966ea2443c7
Michael Schulte-Cörne <michaelmbuso@schule.koeln>
2019-09-13 13:20:47 +0200

    addition von zwei ganzen zahlen

---
+++ Taschenrechner/taschenrechner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1;
+            int zahl2;
+
+            zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("+");
+            zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            int ergebnis = zahl1 + zahl2;
+
+            Console.WriteLine("=\n" + ergebnis);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
